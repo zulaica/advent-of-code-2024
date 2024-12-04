aea17beb234b64d576569cc431ea3d4ca3c882f1
David Zulaica <zulaica@gmail.com>
2024-12-03 18:51:22 -0800

    Day Two, Part Two

--- day-02/index.mjs
+++ day-02/index.mjs
@@ -2,41 +2,63 @@
 
 import { open } from "fs/promises";
 
-const file = await open(new URL("input.txt", import.meta.url));
-
-const reports = [];
-
-for await (const line of file.readLines()) {
-  reports.push(line.split(/\s+/).map(Number));
-}
+const reports = await parseInput();
 
 let safeReports = 0;
 
 for (const report of reports) {
-  const isAscending =
-    JSON.stringify(report) === JSON.stringify(report.toSorted((a, b) => a - b));
-  const isDescending =
-    JSON.stringify(report) === JSON.stringify(report.toSorted((a, b) => b - a));
-  let isSafe = false;
-
-  if (isAscending || isDescending) {
-    for (const index of report.keys()) {
-      const difference = Math.abs(report[index] - report[index + 1]);
-
-      if (Number.isNaN(difference)) break;
-
-      if (difference >= 1 && difference <= 3) {
-        isSafe = true;
-      } else {
-        isSafe = false;
-        break;
-      }
-    }
-  }
-
-  if (isSafe) {
-    safeReports += 1;
-  }
+  safeReports += Number(isSafe(report));
 }
 
 console.info(`Safe Reports: ${safeReports}`);
+
+let safeReportsWithDampener = 0;
+
+for (const report of reports) {
+  const status = [];
+
+  for (const index of report.keys()) {
+    status.push(isSafe(report.toSpliced(index, 1)));
+  }
+
+  safeReportsWithDampener += status.some((isTrue) => isTrue);
+}
+
+console.info(`Safe Reports with Dampener: ${safeReportsWithDampener}`);
+
+async function parseInput() {
+  const file = await open(new URL("input.txt", import.meta.url));
+  const lines = [];
+
+  for await (const line of file.readLines()) {
+    lines.push(line.split(/\s+/).map(Number));
+  }
+
+  return lines;
+}
+
+function isAscending(report) {
+  return (
+    JSON.stringify(report) === JSON.stringify(report.toSorted((a, b) => a - b))
+  );
+}
+
+function isDescending(report) {
+  return (
+    JSON.stringify(report) === JSON.stringify(report.toSorted((a, b) => b - a))
+  );
+}
+
+function isSafe(report) {
+  if (!isAscending(report) && !isDescending(report)) return false;
+
+  const differences = [];
+
+  for (const index of report.keys()) {
+    if (report[index + 1]) {
+      differences.push(Math.abs(report[index] - report[index + 1]));
+    }
+  }
+
+  return differences.every((difference) => difference >= 1 && difference <= 3);
+}
